051939e836bd4668e735a1aba94971107d2057e6
bpotts11 <brittneympotts@gmail.com>
2021-03-18 12:06:59 -0500

    finished setting up app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import AxeGame from './AxeGame';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<AxeGame />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
