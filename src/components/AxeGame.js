5c95e88964a4c7d39ac1cdc2eee29b457c31877e
bpotts11 <brittneympotts@gmail.com>
2021-03-18 15:45:35 -0500

    created a basic navbar. links seem to work so far

--- src/components/AxeGame.js
+++ src/components/AxeGame.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar";
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
 import { userStorageKey } from "./auth/authSettings"
@@ -12,6 +13,7 @@ export const AxeGame = () => (
       if (sessionStorage.getItem(userStorageKey)) {
         return (
           <>
+            <NavBar />
             <ApplicationViews />
           </>
         )
