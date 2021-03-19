d4a6d5ba80d7a9c8aa9df5fdddf0f795158219ea
bpotts11 <brittneympotts@gmail.com>
2021-03-18 22:09:15 -0500

    created post/add match fetch call

--- src/components/match/MatchProvider.js
+++ src/components/match/MatchProvider.js
@@ -1,1 +1,25 @@
 import React, { useState, createContext } from "react"
+
+export const MatchContext = createContext()
+
+export const MatchProvier = (props) => {
+    const [matches, setMatches] = useState([])
+
+    const addMatches = matchObj => {
+        return fetch("http://localhost:8088/matches", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(matchObj)
+        })
+    }
+
+    return (
+        <MatchContext.Provider value={{
+            matches, addMatches
+        }}>
+            {props.children}
+        </MatchContext.Provider>
+    )
+}
